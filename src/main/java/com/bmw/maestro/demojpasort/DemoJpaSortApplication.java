afbb75b5a6ef675644b8d14b9bbd24f92822a699
maurigvs <maurigvs@icloud.com>
2024-12-03 15:22:13 +0000

    initial commit

---
+++ src/main/java/com/bmw/maestro/demojpasort/DemoJpaSortApplication.java
@@ -0,0 +1,13 @@
+package com.bmw.maestro.demojpasort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpaSortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJpaSortApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bmw/maestro/demojpasort/DemoJpaSortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmw.maestro.demojpasort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJpaSortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
